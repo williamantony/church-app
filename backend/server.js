6c1f293ea06577186a08d267d4af4ef0fe5c64d6
William Antony <william@pathadan.com>
2018-06-04 18:32:53 -0500

    Initialized Express & Mongoose

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const {
+  PORT,
+  DBUSER,
+  DBPASSWORD,
+} = process.env;
+
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const server = express();
+
+// server.use(morgan('combined'));
+server.use(cors());
+server.use(bodyParser.json());
+
+mongoose.Promise = global.Promise;
+
+const DBPATH = `mongodb://${DBUSER}:${DBPASSWORD}@ds147890.mlab.com:47890/orgman`;
+mongoose.connect(DBPATH, {}, () => {
+  console.log(`Database is running`);
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
