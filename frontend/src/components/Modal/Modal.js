45f754a56ed3ec0f21e453309bb9bb0f96590edd
William Antony <william@pathadan.com>
2018-06-20 10:48:14 -0500

    Created Modal Component

---
+++ frontend/src/components/Modal/Modal.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { hideModal} from '../../redux/actions';
+import './Modal.css';
+
+class Modal extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      type: props.type || 'general',
+      name: props.name || Date.now(),
+      isVisible: props.isVisible || false,
+      content: props.content || null,
+      color: {
+        bg: 'rgba(0, 0, 0, 0.75)',
+      },
+    };
+  }
+
+  componentWillReceiveProps(props) {
+    this.setState({
+      ...props[this.state.name],
+    });
+  }
+
+  hideModal = () => {
+    this.props.hideModal(this.state.name);
+  }
+
+  render() {
+    const { isVisible, type, content, color } = this.state;
+
+    return (
+      <div className="Modal" data-visible={isVisible} data-type={type}>
+        <div className="Modal__bg" style={{ backgroundColor: color.bg }} onClick={this.hideModal} />
+        <div className="Modal__content">
+          <div className="Modal__content__box">{content}</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return state.modal;
+};
+
+const mapDispatchToProps = {
+  hideModal,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Modal);
