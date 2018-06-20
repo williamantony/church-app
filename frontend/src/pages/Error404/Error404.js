27d24d91481d2144122e8d3356631b881f0c2778
William Antony <william@pathadan.com>
2018-06-20 10:55:45 -0500

    Created Error404 page Component

---
+++ frontend/src/pages/Error404/Error404.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Error404.css';
+
+class Error404 extends Component {
+
+  render() {
+    return (
+      <div className="Error404">
+        Not Found
+      </div>
+    );
+  }
+
+}
+
+export default Error404;
