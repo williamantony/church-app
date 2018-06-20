f808edef131b51d27e6085baea0402a405b76173
William Antony <william@pathadan.com>
2018-06-20 10:57:09 -0500

    Connected Redux, Added Routes to index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import ReduxPromise from 'redux-promise';
+import ReduxThunk from 'redux-thunk';
+import Reducers from './redux/reducers';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import './index.css';
+import SignUp from './pages/SignUp/SignUp';
+import SignIn from './pages/SignIn/SignIn';
+import Error404 from './pages/Error404/Error404';
+import Modal from './components/Modal/Modal';
+
+const ReduxStore = createStore(Reducers, applyMiddleware(ReduxPromise, ReduxThunk));
+
+ReactDOM.render(
+  <Provider store={ReduxStore}>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/register" component={SignUp} />
+          <Route exact path="/login" component={SignIn} />
+          <Route component={Error404} />
+        </Switch>
+        <Modal name="InputSelectorModal" type="InputSelector" />
+        <Modal name="LoaderModal" type="Loader" />
+      </div>
+    </Router>
+  </Provider>,
+  document.getElementById('root')
+);
