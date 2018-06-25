3e0107f8aba31d6182cf7544509e11eba2d1f92f
William Antony <william@pathadan.com>
2018-06-24 23:54:05 -0500

    Created Modal group Component

--- frontend/src/components/Modal/Modal.js
+++ frontend/src/components/Modal/Modal.js
@@ -1,58 +1,43 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { hideModal } from '../../redux/actions';
+import { } from '../../redux/actions';
 import './Modal.css';
+import ModalInstance from '../ModalInstance/ModalInstance';
 
 class Modal extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      type: props.type || 'general',
-      name: props.name || Date.now(),
-      isVisible: props.isVisible || false,
-      content: props.content || null,
-      color: {
-        bg: 'rgba(0, 0, 0, 0.75)',
-      },
+      modals: props.modals,
     };
   }
 
   componentWillReceiveProps(props) {
-    const modal = props[this.state.name];
-    this.setState({ ...modal });
-    setTimeout(() => {
-      if (modal && !modal.isVisible) {
-        this.setState({ content: null });
-      }
-    }, 250);
-  }
-
-  hideModal = () => {
-    this.props.hideModal(this.state.name);
+    this.setState({
+      modals: props.modals,
+    });
   }
 
   render() {
-    const {
-      isVisible, type, content, color,
-    } = this.state;
-
     return (
-      <div className="Modal" data-visible={isVisible} data-type={type}>
-        <div className="Modal__bg" style={{ backgroundColor: color.bg }} onClick={this.hideModal} />
-        <div className="Modal__content">
-          <div className="Modal__content__box">{ content }</div>
-        </div>
+      <div className="Modals">
+        {
+          Object.keys(this.state.modals || {}).map((modal) => {
+            return <ModalInstance key={modal} id={modal} />;
+          })
+        }
       </div>
     );
   }
+
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return state.modal;
 };
 
 const mapDispatchToProps = {
-  hideModal,
+  
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Modal);
