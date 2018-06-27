b2757dc05138338cdb40fad418245f1d33f0582c
William Antony <william@pathadan.com>
2018-06-27 12:34:55 -0500

    Added storage to reducer

--- frontend/src/redux/reducers/index.js
+++ frontend/src/redux/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
+import storage from './storage.reducer';
 import form from './form.reducer';
 import modal from './modal.reducer';
 import user from './user.reducer';
 
 export default combineReducers({
+  storage,
   form,
   modal,
   user,
