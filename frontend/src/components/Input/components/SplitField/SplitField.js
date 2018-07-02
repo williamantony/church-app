c3dfe3745e801e0f5bda10ff42c78617308e55bd
William Antony <william@pathadan.com>
2018-07-02 14:28:37 -0500

    Modified SplitField Component to resemble Input style

--- frontend/src/components/Input/components/SplitField/SplitField.js
+++ frontend/src/components/Input/components/SplitField/SplitField.js
@@ -28,30 +28,35 @@ class SplitField extends Component {
 
   render() {
     return (
-      <div className="SplitField">
-        {
-          (this.state.label)
-            ? <div className="SplitField__label">{this.state.label}</div>
-            : null
-        }
-        <div className="SplitField__input-set">
-          {
-            this.state.fields.map((field, index, fields) => {
-              return (
-                <SplitFieldInput
-                  key={new Date().getTime() + index + 1}
-                  reference={field.ref}
-                  nextInputField={fields[index + 1]}
-                  id={`${this.state.fieldId}_${index + 1}`}
-                  form={this.state.form}
-                  name={this.state.name}
-                  type={field.type}
-                  label={field.label}
-                  length={field.length}
-                />
-              );
-            })
-          }
+      <div className="Input" data-focused="false">
+        <div className="Input__area">
+          <div className="Input__layer-box" />
+          <div className="SplitField">
+            {
+              (this.state.label)
+                ? <div className="SplitField__label">{this.state.label}</div>
+                : null
+            }
+            <div className="SplitField__input-set">
+              {
+                this.state.fields.map((field, index, fields) => {
+                  return (
+                    <SplitFieldInput
+                      key={new Date().getTime() + index + 1}
+                      reference={field.ref}
+                      nextInputField={fields[index + 1]}
+                      id={`${this.state.fieldId}_${index + 1}`}
+                      form={this.state.form}
+                      name={this.state.name}
+                      type={field.type}
+                      label={field.label}
+                      length={field.length}
+                    />
+                  );
+                })
+              }
+            </div>
+          </div>
         </div>
       </div>
     );
