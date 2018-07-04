ef7b656cffd55e12e30692b1f708f530a25730a6
William Antony <william@pathadan.com>
2018-07-04 10:05:40 -0500

    Created addPerson() in Person.controller

--- backend/api/Person/Person.controller.js
+++ backend/api/Person/Person.controller.js
@@ -1,9 +1,14 @@
 const Person = require('./Person.model');
 
 const addPerson = async (req, res) => {
-  
-
-
+  const { person } = req.body;
+  try {
+    const createRequest = await Person.create(person);
+    res.status(201).json(createRequest);
+  }
+  catch(error) {
+    console.log(error);
+  }
 };
 
 const getPerson = (req, res) => {
@@ -15,7 +20,7 @@ const getPerson = (req, res) => {
 const updatePerson = async (req, res) => {
   
 
-  
+
 };
 
 const deletePerson = (req, res) => {
