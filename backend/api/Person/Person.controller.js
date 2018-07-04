2e85cf5fc382228afeb5ec1978c1843fddab5a96
William Antony <william@pathadan.com>
2018-07-04 10:04:30 -0500

    Created a boilerplate for Person.controller

---
+++ backend/api/Person/Person.controller.js
@@ -0,0 +1,32 @@
+const Person = require('./Person.model');
+
+const addPerson = async (req, res) => {
+  
+
+
+};
+
+const getPerson = (req, res) => {
+  
+
+
+};
+
+const updatePerson = async (req, res) => {
+  
+
+  
+};
+
+const deletePerson = (req, res) => {
+// Never Delete Record
+
+
+};
+
+module.exports = {
+  addPerson,
+  getPerson,
+  updatePerson,
+  deletePerson,
+};
