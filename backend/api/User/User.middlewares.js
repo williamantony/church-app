bf460a5b2c30c9dbad5167f54ff767de8ff79da4
William Antony <william@pathadan.com>
2018-07-04 16:15:49 -0500

    Created validatePasswordStrength() in User.middlewares

--- backend/api/User/User.middlewares.js
+++ backend/api/User/User.middlewares.js
@@ -30,7 +30,28 @@ const validateUsername = (req, res, next) => {
   next();
 };
 
+const validatePasswordStrength = (req, res, next) => {
+  const { password } = req.body;
+
+  if (password.length < 8) {
+    res.status(USER_ERROR).json({
+      error: 'Password should have alteast 6 characters',
+    });
+    return;
+  }
+
+  if (!new RegExp(/\d/g).test(password)) {
+    res.status(USER_ERROR).json({
+      error: 'Password should contain number(s)',
+    });
+    return;
+  }
+
+  next();
+};
+
 module.exports = {
   validateEmailAddress,
   validateUsername,
+  validatePasswordStrength,
 };
