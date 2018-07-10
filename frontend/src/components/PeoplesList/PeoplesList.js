65f8d508f3d46670317b8c2cfc7e62d15c8d9ddb
William Antony <william@pathadan.com>
2018-07-09 22:49:54 -0500

    Created PeoplesList Component

---
+++ frontend/src/components/PeoplesList/PeoplesList.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getPeople } from '../../redux/actions';
+import './PeoplesList.css';
+import PeopleCard from '../PeopleCard/PeopleCard';
+
+class PeoplesList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      peoples: [],
+    };
+  }
+
+  componentWillMount() {
+    this.props.getPeople({});
+  }
+
+  componentWillReceiveProps(props) {
+    this.setState({
+      peoples: [
+        ...props.people,
+      ],
+    });
+  }
+
+  render() {
+    return (
+      <div className="PeoplesList">
+        {
+          this.state.peoples.map((people) => {
+            return <PeopleCard key={people._id} people={people} />;
+          })
+        }
+      </div>
+    );
+  }
+
+}
+
+const mapStateToProps = (state) => {
+  return {
+    people: state.people,
+  };
+};
+
+const mapDispatchToProps = {
+  getPeople,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(PeoplesList);
