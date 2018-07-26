2c71ae71f09366dfc9a67cb1da3b07d868c1db1f
William Antony <william@pathadan.com>
2018-07-25 23:12:41 -0500

    Created ProfileEditActions Component

---
+++ frontend/src/components/Profile/components/ProfileEditActions/ProfileEditActions.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import './ProfileEditActions.css';
+
+class ProfileEditActions extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+  
+  render() {
+    return (
+      <div className="ProfileEditActions">
+        <div className="ProfileEditActions__button-group" data-type="edit">
+          <div className="ProfileEditActions__button" data-type="edit">Edit Profile</div>
+        </div>
+        <div className="ProfileEditActions__button-group" data-type="save-discard">
+          <div className="ProfileEditActions__button" data-type="discard">Discard</div>
+          <div className="ProfileEditActions__button" data-type="save">Save</div>
+        </div>
+      </div>
+    );
+  }
+
+}
+
+const mapStateToProps = (state) => {
+  return state;
+};
+
+const mapDispatchToProps = {
+
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(ProfileEditActions);
