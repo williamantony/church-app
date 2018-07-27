688f3e262ff1c2b5ff68cd802dff149a9781c89e
William Antony <william@pathadan.com>
2018-07-26 23:37:06 -0500

    Modified Profile Component

--- frontend/src/components/Profile/Profile.js
+++ frontend/src/components/Profile/Profile.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import './Profile.css';
+import ProfileAddress from './components/ProfileAddress/ProfileAddress';
+import ProfilePhone from './components/ProfilePhone/ProfilePhone';
+import ProfileEmail from './components/ProfileEmail/ProfileEmail';
+import ProfileEditActions from './components/ProfileEditActions/ProfileEditActions';
 
 class Profile extends Component {
   constructor(props) {
@@ -17,24 +21,65 @@ class Profile extends Component {
       <div className="Profile">
         <div className="Profile__title">Profile Information</div>
         <div className="ProfileSection">
-          <div className="ProfileSection__title">Address</div>
+          <div className="ProfileSection__head">
+            <div className="ProfileSection__title">Address</div>
+            <div className="ProfileSection__head-button">
+              edit
+            </div>
+          </div>
           {
             profile.address.map((address) => {
-              return (
-                <div className="ProfileField" key={address._id} >
-                  <div className="ProfileField__title">{ address.type }</div>
-                  <div className="ProfileFieldItem">
-                    <div className="ProfileFieldItem__row">
-                      <div className="ProfileFieldItem__input" />
-                      <div className="ProfileFieldItem__input" />
-                      <div className="ProfileFieldItem__input" />
-                    </div>
-                  </div>
-                </div>
-              );
+              return <ProfileAddress key={address._id} address={address} />;
+            })
+          }
+          {
+            // This is only for demo
+            [0].map((address, index) => {
+              return <ProfileAddress key={address._id + index} address={address} />;
             })
           }
         </div>
+
+        <div className="ProfileSection">
+          <div className="ProfileSection__head">
+            <div className="ProfileSection__title">Phone Number</div>
+            <div className="ProfileSection__head-button">
+              edit
+            </div>
+          </div>
+          {
+            profile.phone.map((phone) => {
+              return <ProfilePhone key={phone._id} phone={phone} />;
+            })
+          }
+          {
+            // This is only for demo
+            [0].map((phone, index) => {
+              return <ProfilePhone key={phone._id + index} phone={phone} />;
+            })
+          }
+        </div>
+
+        <div className="ProfileSection">
+          <div className="ProfileSection__head">
+            <div className="ProfileSection__title">Email Address</div>
+            <div className="ProfileSection__head-button">
+              edit
+            </div>
+          </div>
+          {
+            profile.emailAddress.map((email) => {
+              return <ProfileEmail key={email._id} email={email} />;
+            })
+          }
+          {
+            // This is only for demo
+            [0].map((email, index) => {
+              return <ProfileEmail key={email._id + index} email={email} />;
+            })
+          }
+        </div>
+        <ProfileEditActions />
       </div>
     );
   }
