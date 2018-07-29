f55f83731c2e41319a65114211686b1784f5f094
William Antony <william@pathadan.com>
2018-07-28 23:27:57 -0500

    Modified styles and PeopleListItem

--- frontend/src/components/PeoplesList/components/PeoplesListItem/PeoplesListItem.js
+++ frontend/src/components/PeoplesList/components/PeoplesListItem/PeoplesListItem.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { showModal } from '../../../../redux/actions';
 import './PeoplesListItem.css';
-import PeopleCard from '../../../PeopleCard/PeopleCard';
+import Profile from '../../../Profile/Profile';
 
 class PeoplesListItem extends Component {
   constructor(props) {
@@ -21,21 +21,19 @@ class PeoplesListItem extends Component {
     };
   }
 
-  showPeopleCard = (event) => {
+  showProfileInfo = (event) => {
     event.preventDefault();
 
     const modalId = 'PeoplesListItem__modal';
-    const content = <PeopleCard person={this.state.person} />;
-    const options = {
-      title: 'Profile Information',
-    };
+    const content = <Profile person={this.state.person} />;
+    const options = {};
 
     this.props.showModal(content, modalId, options);
   }
 
   render() {
     return (
-      <div className="PeoplesListItem" onClick={this.showPeopleCard}>
+      <div className="PeoplesListItem" onClick={this.showProfileInfo}>
         <div className="PeoplesListItem__summary">
           <div className="PeoplesListItem__name">{ this.state.displayName }</div>
           <div className="PeoplesListItem__subtext">{ this.state.otherName }</div>
